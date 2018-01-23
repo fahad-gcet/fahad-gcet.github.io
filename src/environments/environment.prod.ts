c61c68bc567011389def54e8304420e9497abbd7
fahad-gcet <fms525@outlook.com>
2018-01-23 18:34:05 +0530

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyD45Fl0ujG-iOw5vNzCL-eLUA9S7PdLOXI",
+        authDomain: "angular5-firebase-crud.firebaseapp.com",
+        databaseURL: "https://angular5-firebase-crud.firebaseio.com",
+        projectId: "angular5-firebase-crud",
+        storageBucket: "angular5-firebase-crud.appspot.com",
+        messagingSenderId: "72557190907"
+    }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyD45Fl0ujG-iOw5vNzCL-eLUA9S7PdLOXI",
+        authDomain: "angular5-firebase-crud.firebaseapp.com",
+        databaseURL: "https://angular5-firebase-crud.firebaseio.com",
+        projectId: "angular5-firebase-crud",
+        storageBucket: "angular5-firebase-crud.appspot.com",
+        messagingSenderId: "72557190907"
+    }
 };
